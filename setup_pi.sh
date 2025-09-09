ae60d602cd0508d5c7f6f7d3fc2f6a2f89fc556b
Harrison <hpaige422@gmail.com>
2025-09-08 23:51:53 -0500

    EGL

--- setup_pi.sh
+++ setup_pi.sh
@@ -123,6 +123,7 @@ install_packages() {
         
         # Graphics and display
         libgl1-mesa-dri libgles2 libopengl0 mesa-utils libegl1 mesa-vulkan-drivers
+        mesa-opencl-icd ocl-icd-opencl-dev libgles2-mesa-dev libegl-mesa0
         libxcb-cursor0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1
         libxcb-randr0 libxcb-render-util0 libxcb-shape0 libxcb-sync1
         libxcb-xfixes0 libxcb-xinerama0 libxcb-xkb1 libxkbcommon-x11-0 python3-xdg
@@ -251,10 +252,15 @@ User=$USER
 Environment=DISPLAY=:0
 Environment=QT_QPA_PLATFORM=xcb
 Environment=XAUTHORITY=/home/$USER/.Xauthority
-Environment=QTWEBENGINE_CHROMIUM_FLAGS=--disable-web-security --allow-running-insecure-content --disable-gpu-sandbox --no-sandbox --disable-dev-shm-usage --memory-pressure-off --max_old_space_size=128 --memory-reducer --gpu-memory-buffer-size-mb=32 --max-tiles-for-interest-area=128 --num-raster-threads=1 --disable-accelerated-video-decode --disable-background-timer-throttling --disable-renderer-backgrounding --disable-features=VizDisplayCompositor,UseSkiaRenderer --disable-gpu-memory-buffer-video-frames --disable-gpu-memory-buffer-compositor-resources --max-tiles-for-interest-area=64
+Environment=QTWEBENGINE_CHROMIUM_FLAGS=--enable-gpu --ignore-gpu-blocklist --enable-accelerated-video-decode --enable-webgl --disable-web-security --allow-running-insecure-content --disable-gpu-sandbox --use-gl=egl --enable-unsafe-webgpu --enable-hardware-overlays --enable-accelerated-video --enable-native-gpu-memory-buffers --enable-zero-copy --disable-backgrounding-occluded-windows --max-tiles-for-interest-area=512 --enable-gpu-rasterization --enable-oop-rasterization --enable-webgl-draft-extensions --enable-webgl-image-chromium --gpu-testing-vendor-id=0xFFFF --gpu-testing-device-id=0xFFFF --no-sandbox --disable-dev-shm-usage --memory-pressure-off --max_old_space_size=128 --memory-reducer --gpu-memory-buffer-size-mb=32 --num-raster-threads=1
 Environment=PYQTGRAPH_QT_LIB=PyQt6
 Environment=QT_DEBUG_PLUGINS=0
 Environment=QT_LOGGING_RULES=qt.qpa.plugin=false
+Environment=MESA_GL_VERSION_OVERRIDE=3.3
+Environment=MESA_GLSL_VERSION_OVERRIDE=330
+Environment=LIBGL_ALWAYS_SOFTWARE=0
+Environment=GALLIUM_DRIVER=v3d
+Environment=VDPAU_DRIVER=v3d
 WorkingDirectory=/home/$USER/Desktop/project
 ExecStart=/usr/bin/python3 /home/$USER/Desktop/project/app.py
 Restart=always
