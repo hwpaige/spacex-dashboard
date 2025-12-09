a1cac916f06fd995603f7b15828b62425d6b0b0c
Harrison <hpaige422@gmail.com>
2025-12-08 22:14:42 -0600

    test update 2

--- src/app.py
+++ src/app.py
@@ -57,7 +57,7 @@ if hasattr(sys.stderr, 'reconfigure'):
     except Exception:
         pass
 
-# test update
+# test update 2
 
 # Environment variables for Qt and Chromium - Force Hardware Acceleration
 if platform.system() == 'Windows':
